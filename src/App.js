59d0ae5e0e178c7e13534bc1ca2eb67b8cc6c2bd
Ünal <unalustk@windowslive.com>
2022-06-11 14:10:19 +0300

    api config

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Coins from "./components/Coins"
 function App() {
     const [coins, setCoins] = useState()
     const [isFetch, setIsFetch] = useState(false)
+    const [options, setOptions] = useState({
+        page: "13",
+    })
+
     useEffect(() => {
         axios
             .get(
-                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=try&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+                `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${options.page}&sparkline=false`
             )
             .then((res) => {
                 setCoins(res.data)
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -5,16 +5,80 @@ export default function Coins(props) {
 
     if (props.isFetch) {
         items = props.coins.map((coin) => {
+            console.log(typeof coin.current_price)
+
+            let coinPrice
+            if (coin.current_price && coin.current_price > 1) {
+                coinPrice = coin.current_price.toLocaleString("en-US")
+            } else if (coin.current_price && coin.current_price < 1) {
+                coinPrice = coin.current_price
+            } else {
+                coinPrice = "Unknown"
+            }
             return (
-                <li key={coin.id}>
-                    {coin.name} <img src={coin.image} alt="" />{" "}
-                    {coin.current_price}=>
-                    {coin.market_cap_rank}
-                </li>
+                <tr key={coin.id}>
+                    <td>{coin.market_cap_rank}</td>
+                    <td>
+                        <img src={coin.image} alt="" /> {coin.name}{" "}
+                        {coin.symbol.toUpperCase()}
+                    </td>
+                    <td>
+                        {/* {coin.current_price ? coin.current_price : "Unknown"} */}
+                        {coinPrice}
+                    </td>
+                    <td>
+                        {coin.price_change_percentage_24h
+                            ? coin.price_change_percentage_24h.toFixed(2) + "%"
+                            : "Unknown"}
+                    </td>
+                    <td>
+                        {coin.market_cap
+                            ? coin.market_cap.toLocaleString("en-US")
+                            : "Unknown"}
+                    </td>
+                    <td>
+                        {coin.max_supply
+                            ? coin.max_supply.toLocaleString(undefined, {
+                                  minimumFractionDigits: 0,
+                              }) +
+                              " " +
+                              coin.symbol.toUpperCase()
+                            : " Unknown"}
+                    </td>
+                    <td>
+                        {coin.circulating_supply
+                            ? coin.circulating_supply.toLocaleString("en-US") +
+                              " " +
+                              coin.symbol.toUpperCase()
+                            : "Unknown"}
+                    </td>
+                </tr>
             )
         })
     } else {
-        items = "not fetched"
+        items = (
+            <tr>
+                <td>not fetched</td>
+            </tr>
+        )
     }
-    return <div>{items}</div>
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>24h %</th>
+                        <th>Market Cap</th>
+                        <th>Max Supply</th>
+                        <th>Circulating Supply</th>
+                    </tr>
+                </thead>
+                <tbody>{items}</tbody>
+            </table>
+        </div>
+    )
 }
